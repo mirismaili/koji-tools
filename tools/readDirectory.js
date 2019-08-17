aecfe04d1442ded4b0a8db8102d771d89ba7e5b5
S. M. Mir-Ismaili <s.m.mirismaili@gmail.com>
2019-08-17 18:59:21 +0430

    perf(readDirectory): A safer and faster algorithm was used based on git's built-in interface
    1. A safer, faster and more concise algorithm was used based on git's built-in interface
        (`git ls-files --recurse-submodules`).
    2. An error handler was added to prevent illegal values for `directory` argument

--- tools/readDirectory.js
+++ tools/readDirectory.js
@@ -1,53 +1,30 @@
+'use strict'
 const {execSync} = require('child_process')
 const {_YLW, RED, RST} = require('ansi-colors-and-styles')
+const fs = require('fs')
 const path = require('path')
 
-// Get all git-indexed paths to find koji files
-module.exports = directory => readDirectoryRelative(directory).map(
-    // Make path absolute:
-    relativePath => path.resolve(directory, relativePath)
-)
-
-function readDirectoryRelative(directory) {
+// Get all git-indexed paths
+module.exports = directory => {
+  if (!fs.statSync(directory).isDirectory()) throw Error(`"${directory}" is not a directory!`)
+  
+  let outputBuffer
   try {
-    let list = execSync('git ls-files', {cwd: directory}).toString()
-        .replace(/\n$/, '')
-        .split('\n')
-    
-    // Find the paths of git submodules (not recursive):
-    const submodulesInfo = execSync('git submodule status', {cwd: directory}).toString()
-    
-    const regExp = /^ [A-Fa-f0-9]{40,64} (.+?) \(.+?\)$/gm   // live: https://regex101.com/r/yUEJNe/2/
-    
-    while (true) {
-      const match = regExp.exec(submodulesInfo)
-      
-      if (match === null) break
-      
-      //const submoduleInfo = match[0]
-      const submodulePath = match[1]
-      
-      // Sample values for above variables:
-      //     submoduleInfo: " debd72fe632d7315be8e31fe00c7e767c423a01f sub-repo1 (heads/master)"
-      //     submodulePath: "sub-repo1"
-      
-      // Exclude submodule directory from the list:
-      list = list.filter(path => path !== submodulePath)
-      
-      // Instead, add paths under the submodule (recursive):
-      list.push(...readDirectoryRelative(path.resolve(directory, submodulePath)).map(
-          // Make path relative to `directory`:
-          nestedPath => path.join(submodulePath, nestedPath))
-      )
-    }
-    
-    return list
+    outputBuffer = execSync('git ls-files --recurse-submodules', {cwd: directory})
   } catch (err) {
     const error = new Error(err.message +
-        `\n${_YLW}${RED}Have you installed "git" and added it to the "PATH"? If no see: ` +
-        `https://git-scm.com/book/en/v2/Getting-Started-Installing-Git${RST}\n`
+        `\n${_YLW}${RED}` +   // Red on Yellow bg
+        `Have you installed "git" and added it to the "PATH"? If no see: ` +
+        `https://git-scm.com/book/en/v2/Getting-Started-Installing-Git\n` +
+        `Also, make sure the directory (${directory}) is a git repository.` +
+        `${RST}\n`
     )
     error.stack = err.stack
     throw error
   }
+  
+  return outputBuffer.toString()
+      .replace(/\n$/, '')   // remove last empty line
+      .split('\n')          // convert String to Array
+      .map(relativePath => path.resolve(directory, relativePath))  // convert relative paths to absolute paths and make them OS-specific
 }
